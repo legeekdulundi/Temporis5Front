4276fac8707ae407ff0a9187d6aed6275cf40e09
tituse <titusse3.rateaux@gmail.com>
2021-04-01 21:47:38 +0200

    Modif NavBar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,7 +2,7 @@ const NavBar = () => {
     return (
         <nav >
             <input className="input-navBare" placeholder="Recherche items"></input>
-            <button className="button-navBare" >Recherche</button>
+            <button className="button-navBare" >Recherche eeeeee</button>
         </nav>
     )
 }
